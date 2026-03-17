67018c5aebb44c673f0c9be5a5e0d684dff42f57
Alexander Van Hecke <alexander@avhconsult.be>
2026-03-17 13:55:20 +0100

    try using same prefix for tmux

--- run_once_install-tpm.sh
+++ run_once_install-tpm.sh
@@ -4,4 +4,8 @@ if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-~/.tmux/plugins/tpm/bin/install_plugins
+# Start a detached tmux session, source the config, and install plugins
+tmux new-session -d -s plugin_install
+tmux source-file ~/.tmux.conf
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+tmux kill-session -t plugin_install
