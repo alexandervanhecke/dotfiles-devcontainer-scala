79d9e58d0ab9269cedb9ce7601587b94b095267b
Alexander Van Hecke <alexander@avhconsult.be>
2026-03-18 13:02:33 +0100

    only enable vim tmux when inside tmux

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,12 @@ end, { desc = "LSP Code Action" })
 vim.keymap.set("n", "<C-b>", vim.lsp.buf.definition, { desc = "Go to Definition" })
 vim.keymap.set("n", "<M-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move buffer left" })
 vim.keymap.set("n", "<M-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move buffer right" })
+-- only enable tmux navigation if we're inside a tmux session
+if vim.env.TMUX then
+  -- vim-tmux-navigator handles it
+else
+  vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+  vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+  vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+  vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+end
